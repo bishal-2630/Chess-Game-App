ddd6e09c82bde41d121d33ffe67f259046b1398c
Bishal <kbishal177@gmail.com>
2026-01-15 15:51:22 +0545

    Updated database to PostgreSQL

--- chess_backend/settings.py
+++ chess_backend/settings.py
@@ -4,6 +4,7 @@ Simple Django settings for Termux with Swagger
 import os
 from pathlib import Path
 from decouple import config
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -112,10 +113,11 @@ CHANNEL_LAYERS = {
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
