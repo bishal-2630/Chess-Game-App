887f8909f8f1decebf19e4536f378e2551c21527
Bishal <kbishal177@gmail.com>
2026-01-16 21:49:42 +0545

    Create ultra-minimal handler to eliminate all syntax issues

--- api/index.py
+++ api/index.py
@@ -1,17 +1,4 @@
-"""
-Vercel serverless function handler for Flutter application
-"""
-from django.http import HttpResponse
-
 def handler(environ):
-    """Standard Vercel handler with environ parameter"""
-    try:
-        # Get path from query string or path info
-        path_info = environ.get('PATH_INFO', '').lstrip('/')
-        
-        return HttpResponse("Handler working! Path: " + path_info, content_type='text/html')
-    except Exception as e:
-        return HttpResponse(f"Error: {str(e)}", status=500)
+    return HttpResponse("Working: " + str(environ.get('PATH_INFO', '/')), content_type='text/html')
 
-# Vercel Python runtime looks for 'app' variable
 app = handler
