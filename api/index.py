70602b813b3aaf1846739da783db7d498afdd6e5
Bishal <kbishal177@gmail.com>
2026-03-17 13:28:57 +0545

    debug: remove django from index.py to test environment

--- api/index.py
+++ api/index.py
@@ -1,11 +1,8 @@
-from django.http import HttpResponse
-
-def handler(environ, start_response=None):
-    if start_response:
-        status = '200 OK'
-        headers = [('Content-Type', 'text/html')]
-        start_response(status, headers)
-        return [b"Working: Django environment might be available"]
-    return HttpResponse("Working: Simple Handler", content_type='text/html')
+def handler(environ, start_response):
+    status = '200 OK'
+    headers = [('Content-Type', 'text/html')]
+    start_response(status, headers)
+    import sys
+    return [f"WSGI Working. Python version: {sys.version}".encode('utf-8')]
 
 app = handler
